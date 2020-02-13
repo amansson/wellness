d8ac4b48a4a0c6ed88900c3c53a1a0f2cd2cc7d1
andreas <andreas.mansson1@ingka.ikea.com>
2020-02-13 14:16:50 +0100

    add middleware to find activity with id add delete of activity with id change get to use middleware

--- routes/api/activityApiRouter.js
+++ routes/api/activityApiRouter.js
@@ -3,10 +3,6 @@ const express = require('express');
 function routes(Activity) {
   const activityRouter = express.Router();
 
-  activityRouter.use('/api/', (req, res, next) => {
-    // middleware
-    next();
-  });
 
   activityRouter.route('/api/activity')
     .get((req, res) => {
@@ -23,13 +19,27 @@ function routes(Activity) {
       return res.status(201).json(activity);
     });
 
+  activityRouter.use('/api/activity/:activityId', (req, res, next) => {
+    Activity.findById(req.params.activityId, (err, activity) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (activity) {
+        req.activity = activity;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+
   activityRouter.route('/api/activity/:activityId')
-    .get((req, res) => {
-      Activity.findById(req.params.activityId, (err, activity) => {
+    .get((req, res) => res.json(req.activity))
+    .delete((req, res) => {
+      req.activity.remove((err) => {
         if (err) {
           return res.send(err);
         }
-        return res.json(activity);
+        return res.sendStatus(204);
       });
     });
 
